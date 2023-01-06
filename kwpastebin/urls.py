3b22c19190d424abb19df01c1e4e97b00d5be82b
Chris Warrick <kwpolska@gmail.com>
2023-01-06 17:28:34 +0100

    Django 4.1.x support

--- kwpastebin/urls.py
+++ kwpastebin/urls.py
@@ -1,16 +1,16 @@
-from django.conf.urls import url
+from django.urls import re_path
 from kwpastebin import views
 
 app_name = "kwpastebin"
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^mine/$', views.my_pastes, name='my_pastes'),
-    url(r'^all/$', views.all_pastes, name='all_pastes'),
-    url(r'^invalidate_cache/$', views.invalidate_cache, name='invalidate_cache'),
-    url(r'^(?P<id>.*?)/edit/$', views.edit_paste, name='edit_paste'),
-    url(r'^(?P<id>.*?)/delete/$', views.delete_paste, name='delete_paste'),
-    url(r'^(?P<id>.*?)/invalidate_cache/$', views.invalidate_cache_for_paste, name='invalidate_cache_for_paste'),
-    url(r'^(?P<id>.*?)/raw/$', views.show_paste_raw, name='show_paste_raw'),
-    url(r'^(?P<id>.*?)/$', views.show_paste, name='show_paste'),
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^mine/$', views.my_pastes, name='my_pastes'),
+    re_path(r'^all/$', views.all_pastes, name='all_pastes'),
+    re_path(r'^invalidate_cache/$', views.invalidate_cache, name='invalidate_cache'),
+    re_path(r'^(?P<id>.*?)/edit/$', views.edit_paste, name='edit_paste'),
+    re_path(r'^(?P<id>.*?)/delete/$', views.delete_paste, name='delete_paste'),
+    re_path(r'^(?P<id>.*?)/invalidate_cache/$', views.invalidate_cache_for_paste, name='invalidate_cache_for_paste'),
+    re_path(r'^(?P<id>.*?)/raw/$', views.show_paste_raw, name='show_paste_raw'),
+    re_path(r'^(?P<id>.*?)/$', views.show_paste, name='show_paste'),
 ]
